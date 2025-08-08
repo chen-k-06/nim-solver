52de32bc1d33623272f4d4461745c8994699d3de
Kexin <kkc2160@columbia.edu>
2025-08-08 23:51:49 +0100

    Debug, inital stylesheet

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let values = []
 let inputs = []
 
 document.getElementById("enter1").addEventListener("click", () => {
+    let enter1 = document.getElementById("enter1");
     let value = (document.getElementById("quantity")).value;
     for (let i = 0; i < value; i++) {
         let input = document.createElement("input");
@@ -15,15 +16,18 @@ document.getElementById("enter1").addEventListener("click", () => {
         inputs.push(input)
     }
 
-    console.log("Next round of input created")
+    console.log("Next round of input created");
+    enter1.disabled = true;
+
     let enter2 = document.createElement("button");
     enter2.id = "enter2"
+    enter2.textContent = "Enter"
     document.body.appendChild(enter2);
-});
 
-document.getElementById("enter2").addEventListener("click", () => {
-    for (let i = 0; i < inputs.length; i++) {
-        values.push(inputs[i].value)
-    }
-    console.log("Values: ", values)
+    document.getElementById("enter2").addEventListener("click", () => {
+        for (let i = 0; i < inputs.length; i++) {
+            values.push(inputs[i].value)
+        }
+        console.log("Values: ", values)
+    });
 });
