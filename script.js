9f1d229fbece7bde7c2d50cdce7c83adb6f41615
Kexin <kkc2160@columbia.edu>
2025-08-08 23:40:44 +0100

    Enter1 button event listener

---
+++ script.js
@@ -0,0 +1,29 @@
+let values = []
+let inputs = []
+
+document.getElementById("enter1").addEventListener("click", () => {
+    let value = (document.getElementById("quantity")).value;
+    for (let i = 0; i < value; i++) {
+        let input = document.createElement("input");
+        input.type = "number";
+
+        input.min = "0";
+        input.max = "100";
+        input.step = "1";
+
+        document.body.appendChild(input);
+        inputs.push(input)
+    }
+
+    console.log("Next round of input created")
+    let enter2 = document.createElement("button");
+    enter2.id = "enter2"
+    document.body.appendChild(enter2);
+});
+
+document.getElementById("enter2").addEventListener("click", () => {
+    for (let i = 0; i < inputs.length; i++) {
+        values.push(inputs[i].value)
+    }
+    console.log("Values: ", values)
+});
