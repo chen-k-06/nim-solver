05f27de12e902fdbb2a72beb8d711bf9c5cb3cd3
Kexin <kkc2160@columbia.edu>
2025-08-09 03:36:58 +0100

    completed nim_sum()

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 let values = []
 let inputs = []
 
+function dec2bin(dec) {
+    return (dec >>> 0).toString(2);
+}
+
+function nim_sum(numbers) {
+    binary_nums = [];
+    for (let num of numbers) {
+        binary_nums.push(dec2bin(num))
+    }
+    console.log("binarys: ", binary_nums);
+
+    let result = 0;
+    while (true) {
+        let ones_count = 0;
+        let zero_count = 0;
+
+        // binary XOR
+        for (let i = 0; i < binary_nums.length; i++) {
+            if (binary_nums[i] % 2 == 1) {
+                ones_count++;
+            }
+            binary_nums[i] = Math.floor(binary_nums[i] / 10); // remove last digit of all numbers
+            if (binary_nums[i] == 0) {
+                zero_count++;
+            }
+        }
+
+        result *= 10;
+        if (ones_count % 2 == 1) {
+            result++;
+        }
+        if (zero_count == binary_nums.length) {
+            break;
+        }
+    }
+    console.log("Nim sum result:", result);
+    result = result.toString();
+    const resultStr = result.split("").reverse().join("");
+    return parseInt(resultStr)
+}
+
 document.getElementById("enter1").addEventListener("click", () => {
     let enter1 = document.getElementById("enter1");
     let value = (document.getElementById("quantity")).value;
@@ -25,9 +66,21 @@ document.getElementById("enter1").addEventListener("click", () => {
     document.body.appendChild(enter2);
 
     document.getElementById("enter2").addEventListener("click", () => {
+        values = []
         for (let i = 0; i < inputs.length; i++) {
-            values.push(inputs[i].value)
+            values.push(parseInt(inputs[i].value))
         }
         console.log("Values: ", values)
+        let sum = nim_sum(values);
+        console.log("Sum: ", sum);
+        let result_text = document.createElement("p");
+
+        if (sum == 0) {
+            result_text.textContent = "This is a second player game.";
+        }
+        else {
+            result_text.textContent = "This is a first player game.";
+        }
+        document.body.appendChild(result_text);
     });
 });
